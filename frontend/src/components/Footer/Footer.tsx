1cf21ae1d882d00aa0c444011f0aa1219f8a092e
ltdedtn <34638758+ltdedtn@users.noreply.github.com>
2024-07-15 17:15:47 +1200

    Added footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const theme = localStorage.getItem("theme") || "light";
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    document
+      .querySelector("footer")
+      ?.setAttribute("data-theme", localTheme || "light");
+  }, [theme]);
+
+  return (
+    <div className="navbar bg-base-100 shadow-lg px-4 sm:px-8">
+      <div className="flex-none">Ltdedtn</div>
+      {/* add more here later (socials ect)*/}
+    </div>
+  );
 };
 
 export default Footer;
